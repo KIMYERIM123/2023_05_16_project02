c65ba78a940e1ca3a83fe71287a9d7cbe6b59e9c
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-05-18 18:18:27 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.main_slide').slick({
+        arrows: false,
+        // // vertical: true 위아래 슬라이드
+        // // fade : true 는 흐려짐
+        autoplay: true,
+        // 슬라이드 자동
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+    // 여기까지 슬라이드 하는 방법
+})
