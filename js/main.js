cad443351a470b8c1f7dbda14b647a2a93cd9c5f
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-05-19 13:17:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,42 +1,44 @@
 $(function () {
-    $('.main_slide').slick({
-        arrows: false,
-        // // vertical: true 위아래 슬라이드
-        // // fade : true 는 흐려짐
-        autoplay: true,
-        // 슬라이드 자동
-        pauseOnHover: false,
-        pauseOnFocus: false,
-    });
-    // 여기까지 슬라이드 하는 방법
+  $('.main_slide').slick({
+    arrows: false,
+    // // vertical: true 위아래 슬라이드
+    // // fade : true 는 흐려짐
+    autoplay: true,
+    // 슬라이드 자동
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
+  // 여기까지 슬라이드 하는 방법
 
-    $('.center').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-             centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
-   
-              
+
+
+
+
 
 })
